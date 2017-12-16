3761de62e879e5ab0ab43e233a7a0ad9256cafce
Ammar Iqbal <ammar.iqbal@gmail.com>
2017-12-16 10:42:51 +0100

    -

--- nodestart.sh
+++ nodestart.sh
@@ -5,6 +5,8 @@ if [ $NODE_ENV = "production" ]; then
   echo "starting node"
   node /src/start.js
 else
-  echo "starting nodemon with debugger"
-  nodemon --inspect=0.0.0.0:9003 -L --watch "/src" /src/start.js
+  #echo "starting nodemon with debugger"
+  #nodemon --inspect=0.0.0.0:9003 -L --watch "/src" /src/start.js
+  echo "starting node"
+  node /src/start.js
 fi
