f46542bf1437c8f0675012de2ba41a22774b120f
Ammar Iqbal <ammar.iqbal@gmail.com>
2017-12-17 14:54:42 +0100

    -

--- src/mongohandler.js
+++ src/mongohandler.js
@@ -8,7 +8,7 @@ module.exports.listen = function(url){
 
   //serviceLookupHandler.serviceLookup("userandaccountmongodb", '').then(serverAddress => {
     //var url = config.database;
-    var url = "http://" +  server  +  ":27017/user";
+    var url = "http://" +  server  +  "/user";
     try {
       //mongoose.connect(url);
       mongoose.connect(url, { useMongoClient: true });
